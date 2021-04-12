dae35eba701c8dd4bbc3d9808b747165a345d142
Rodion <r.1on@yandex.ru>
2021-04-12 23:48:18 +0300

    add dist folder

---
+++ src/components/ProductSort.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul class="sort">
+    <li
+      class="sort__item"
+      :class="{ 'sort__item_active': item.active }"
+      v-for="item in list"
+      :key="item.title">{{ item.title }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "ProductSort",
+  data() {
+    return {
+      list: [
+        {
+          title: "Сначала дорогие",
+          active: true,
+        },
+        {
+          title: "Сначала недорогие",
+          active: false,
+        },
+        {
+          title: "Сначала популярные",
+          active: false,
+        },
+        {
+          title: "Сначала новые",
+          active: false,
+        },
+      ],
+    };
+  },
+  }
+</script>
+
+<style scoped>
+
+</style>
